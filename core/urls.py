8b1f76beea19a658653dbce87377cfd9faac414a
Awais <nain.codes@gmail.com>
2023-05-23 17:55:31 +0500

    requirement file and register fields

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main),
-    path('login', views.login_user),
-    path('register', views.register),
+    path('', views.main, name="main"),
+    path('login', views.login_user, name="login"),
+    path('register', views.register, name="register"),
 ]
--- core/views.py
+++ core/views.py
@@ -7,30 +7,41 @@ from .forms import RegisterationForm
 def main(request):
     return render(request, 'main.html')
 
+
 def register(request):
     if request.method == "POST":
-        user_form = RegisterationForm(request.POST)
+        form = RegisterationForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('registed'))
+            return redirect('main')
     else:
-        user_form=RegisterationForm()
-        return render(request, 'register/register.html', {})
+        form = RegisterationForm()
+        return render(request, 'register/register.html', {
+            'form':form,
+        })
     
 
 def login_user(request):
-    if request.method == 'POST':
-        username = request.POST["username"]
-        password = request.POST["password"]
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('main.html')
         else:
-            messages.success(request, ("loggin error"))
+            messages.success(request, ("login error"))
             return redirect('login')
     else:    
         return render(request, 'register/login.html', {})
 
 
-def logout_user(request):
+def logout_view(request):
     logout(request)
+    messages.success(request, ("logged out"))
+    return redirect('login')
