74b88f4442649b4c16488736888cfd0eeeb79310
Zulqarnain Amjad Ali <133605771+Nain-Codes@users.noreply.github.com>
2025-10-27 22:18:43 -0400

    Delete core directory

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main_index, name="main"),
-    path('login', views.login_user, name="login"),
-    path('register', views.register, name="register"),
-]
